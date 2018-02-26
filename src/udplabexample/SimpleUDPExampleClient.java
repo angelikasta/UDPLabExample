596d3d4a06226b80cfa4a1f4ab5a6bda4de61eb2
Matthew <matbulat@gmail.com>
2018-02-26 10:11:18 +0000

    Init Commit

---
+++ src/udplabexample/SimpleUDPExampleClient.java
@@ -0,0 +1,22 @@
+package udplabexample;
+
+import java.io.*;
+import java.net.*;
+
+public class SimpleUDPExampleClient {
+
+    public static void main(String[] args) throws Exception {
+        try (DatagramSocket clientSocket = new DatagramSocket()) {
+            BufferedReader inFromUser
+                    = new BufferedReader(new InputStreamReader(System.in));
+
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] sendData = new byte[1024];
+            String sentence = inFromUser.readLine();
+            sendData = sentence.getBytes();
+            
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendPacket);
+        }
+    }
+}
